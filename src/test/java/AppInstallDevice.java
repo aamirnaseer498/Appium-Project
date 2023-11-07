740dfcc0b1f4b37d68ad1d0252adf2293b5b8633
Aamir Naseer <aamirnaseer498@gmail.com>
2023-11-07 20:59:17 +0500

    Committed APK Installation on Real Device

---
+++ src/test/java/AppInstallDevice.java
@@ -0,0 +1,29 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class AppInstallDevice {
+
+    public static void main(String[] args) {
+
+        UiAutomator2Options uiAutomator2Options= new UiAutomator2Options();
+
+        uiAutomator2Options.setAutomationName("UiAutomator2");
+        uiAutomator2Options.setPlatformName("Android");
+        uiAutomator2Options.setPlatformVersion("13");
+        uiAutomator2Options.setDeviceName("Galaxy A04e");
+        uiAutomator2Options.setApp("C:\\Users\\user\\Downloads\\Snapseed_2.21.0.566275366_Apkpure.apk");
+
+        AndroidDriver androidDriver;
+
+        try {
+            androidDriver= new AndroidDriver(new URL("http://192.168.3.20:4723/"),uiAutomator2Options);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
